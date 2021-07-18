66d169eb2424052523605d0c63625c6c7b7db069
IgnatevD <ignatevds@gmail.com>
2021-07-18 15:11:27 +0300

    Добавлено модальное окно

--- app.js
+++ app.js
@@ -1,65 +1,120 @@
 const galleryItems = [
+    {
+    preview:
+      './images/img/МстителиХалк340.jpg',
+    original:
+      './images/img/МтителиХалк1920х1080.jpg',
+    description: 'Мстители Халк',
+  },
   {
-    preview: 
+    preview:
+      './images/img/Тор340jpg.jpg',
+    original:
+      './images/img/Тор1920х1080.jpg',
+    description: 'Тор',
+  },
+  {
+    preview:
+      './images/img/КапатанМарвел340.jpg',
+    original:
+      './images/img/КапатанМарвел1920х1080.jpg',
+    description: 'Капатан Марвел',
+  },
+ 
+  {
+    preview:
+      './images/img/МстителиТанос340.jpg',
+    original:
+      './images/img/МстителиТанос1920х1080.jpg',
+    description: 'Мстители и Танос',
+  },
+  {
+    preview:
+      './images/img/ДокторСтрендж340.jpg',
+    original:
+      './images/img/ДокторСтрендж1920х1080.jpg',
+    description: 'Доктор Стрендж',
+  },
+ {
+    preview:
+      './images/img/Танос340.jpg',
+    original:
+      './images/img/Танос1920х1080.jpg',
+    description: 'Танос',
+  },
+   {
+    preview:
+      './images/img/МстителиКапитанАм340.jpg',
+    original:
+      './images/img/МстителиКапитанАм1920х1080.jpg',
+    description: 'Капитан Америка',
+  },
+  {
+    preview:
+      './images/img/ТонниСтарк340.jpg',
+    original:
+      './images/img/ТонниСтарк1920х1080.jpg',
+    description: 'Тонни Старк',
+  },
+ 
+  {
+    preview: "./images/img/Грут340.jpg",
     
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
+      './images/img/Грут1920х1080.jpg',
+    description: 'Мстители Грут',
   },
 ];
+
+const galleryList = document.querySelector('.gallery');
+
+
+const modalOpen = document.querySelector('.js-lightbox');
+
+const modalImg = document.querySelector('.lightbox__image');
+console.log(modalImg.src);
+
+
+const modalClose = document.querySelector('.lightbox__button');
+
+
+const gallerLiImg = galleryItems.map(item =>
+  `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${item.original}"
+  >
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+);
+
+
+
+
+galleryList.insertAdjacentHTML('afterbegin', gallerLiImg.join(` `));
+
+galleryList.addEventListener(`click`, onIn => {
+  modalOpen.classList.add(`is-open`);
+  // modalImg.src = `${item.original}`;
+
+});
+
+
+
+modalOpen.addEventListener(`click`, closeMobalBd);
+
+modalClose.addEventListener(`click`, onIn => modalOpen.classList.remove(`is-open`));
+
+
+
+
+function closeMobalBd(e) {
+  if (e.currentTarget === e.target)
+  modalOpen.classList.remove(`is-open`)
+};
